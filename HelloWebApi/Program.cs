1f523923345cf7997b8caa99abb58ab244d8b7c0
Ahmet Faruk TÜRK <af_trk@hotmail.com>
2024-06-09 22:55:19 +0300

    Necessary things related to Swagger added

--- HelloWebApi/Program.cs
+++ HelloWebApi/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    });
 
 var app = builder.Build();
 
@@ -11,7 +16,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
