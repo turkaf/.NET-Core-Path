9aec02cdaf42f03822aa28037c3efce1200ad8a6
Ahmet Faruk TÜRK <af_trk@hotmail.com>
2024-06-09 22:20:30 +0300

    Trials done with Get method

--- HelloWebApi/Controllers/WeatherForecastController.cs
+++ HelloWebApi/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@
 namespace TodoApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]s")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -18,8 +18,34 @@ namespace TodoApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        //api/WheatherForecasts?id=3
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetById([FromQuery] string id)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        //api/WheatherForecasts/3
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetForecast(string id)
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
